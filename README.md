2554f3734582db3e7ce4b18bbe62de09b6c0f2e0
cksk-jp <carlson.ksk@gmail.com>
2023-12-05 15:57:57 -0500

    Base project completed

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # movies-app
 Small app where user can input a movie title and an associated rating. Built using jQuery
+
+# Change-log 2023-12-05
+- Added checks for rating input must be between 0-10
+- Pattern atrribute added to input for string greater than 2
+- Sorting functionality implemented
+
+#TODO
+- Separate styles to separate file
--- app.js
+++ app.js
@@ -1,22 +1,61 @@
 $('form').on('submit', function (e) {
   e.preventDefault();
-
   addEntry($('#movie-title').val(), $('#rating').val());
-
   $('#movie-title').val('');
   $('#rating').val('');
 });
 
-const addEntry = (titleInput, ratingInput) => {
+function addEntry(title, rating) {
   let newEntry = $('<tr>');
-  newEntry.append(`<td>${titleInput}</td>`);
-  newEntry.append(`<td>${ratingInput}</td>`);
+  newEntry.append(`<td>${title}</td>`);
+  newEntry.append(`<td>${rating}</td>`);
   newEntry.append('<td><button class="removeRowBtn">Remove</button></td>');
   newEntry.appendTo('tbody')
 }
 
 $('tbody').on('click', '.removeRowBtn', function () {
   $(this).closest('tr').remove();
+});
+
+/**
+ * 
+ * @param {HTMLTableElement} table to sort 
+ * @param {number} index of column to sort 
+ * @param {boolean} asc true if asc and false for dsc 
+ */
+function sortTableByColumn(column, asc = true) {
+  console.log('sorting...');
+  const dirToggle = asc ? 1 : -1;
+  const rows = $('tbody tr').toArray();
+
+  //sort each row
+  const sortedRows = rows.sort((a, b) => {
+    const aColText = $(a).find('td').eq(column).text().toLowerCase().trim();
+    const bColText = $(b).find('td').eq(column).text().toLowerCase().trim();
+    return aColText > bColText ? (1 * dirToggle) : (-1 * dirToggle);
+  });
+
+  //remove existing rows
+  $('tbody').html('');
+
+  //re-add the newly sorted rows
+  $('tbody').append(...sortedRows);
+
+  $('th').removeClass('th-sort-asc th-sort-dsc');
+
+  $('th').eq(column).addClass(asc ? 'th-sort-asc' : ' th-sort-dsc');
+}
+
+// Movie column
+$('th').eq(0).closest('th').on('click', function () {
+  const currentSort = $(this).hasClass('th-sort-asc');
+  sortTableByColumn(0, !currentSort);
+});
+
+// Rating column
+$('th').eq(1).closest('th').on('click', function () {
+  const currentSort = $(this).hasClass('th-sort-asc');
+  sortTableByColumn(1, !currentSort);
 });
 
 
@@ -37,7 +76,6 @@ $('th, td').css({
 $('form').css({
   'width': '600px',
   'display': 'flex',
-  'flex': 'space-between',
   'gap': '10px',
   'margin': '10px'
 });
